161df449f3be9e96db3be1aa6580e614e268d3c2
Rodrigo <rodrigo.facury14@gmail.com>
2023-07-14 00:35:06 -0300

    feat: inicia página de login

---
+++ src/Pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+import "./Login.css"
+import logo from "../../assets/logo.png"
+
+function Login() {
+  return (
+    <main id="login-page">
+      <h1 id="title">Entre e trace seu caminho!</h1>
+      <img src={logo} alt="Logo da Star Trail" />
+    </main>
+  )
+}
+
+export default Login
