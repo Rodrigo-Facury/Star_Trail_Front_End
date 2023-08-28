d697712a36b149c529e3186d48fa42c7698d2d07
Rodrigo <rodrigo.facury14@gmail.com>
2023-08-28 10:01:48 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-  define: {
-    "process.env": {},
-  },
-})
+    host: true,
+    port: 8000,
+     watch: {
+       usePolling: true
+     }
+  }
+ })
