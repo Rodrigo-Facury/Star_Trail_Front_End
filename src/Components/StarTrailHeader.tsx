df51832fc149d86910c8b6d3bec1bdabbcdabff5
Rodrigo <rodrigo.facury14@gmail.com>
2023-09-21 15:33:09 -0300

    chore: inicia configurações para socket

--- src/Components/StarTrailHeader.tsx
+++ src/Components/StarTrailHeader.tsx
@@ -54,7 +54,7 @@ function StarTrailHeader() {
   const navigate = useNavigate()
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const token: string | number | boolean | object | null = secureLocalStorage.getItem('st_token')
-
+  
   useEffect(() => {
     if (token && typeof token === 'string') {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
